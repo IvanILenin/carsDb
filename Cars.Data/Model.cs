133347647f2fcb18c8fb4c8f9825f4f6e052ca64
ДИТ - Отдел поддержки пользователей НИУ ВШЭ <ditsupport@hse.ru>
2017-12-04 14:05:44 +0300

    Add 3 classes

--- Cars.Data/Model.cs
+++ Cars.Data/Model.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Cars.Data
 {
-    public class Class1
+    public class Model
     {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public Manufacturer manufacturer { get; set; }
     }
 }
